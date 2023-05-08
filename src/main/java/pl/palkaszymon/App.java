617dd70e8fc3acb73a36ed22b51b81b1bdabedbb
palkaszymon <s221437@student.uek.krakow.pl>
2023-05-08 18:40:51 +0200

    24.04 classes

--- src/main/java/pl/palkaszymon/App.java
+++ src/main/java/pl/palkaszymon/App.java
@@ -1,23 +1,32 @@
+
 package pl.palkaszymon;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.palkaszymon.ProductCatalog.ProductCatalog;
+import pl.palkaszymon.ProductCatalog.HashMapProductStorage;
+import java.math.BigDecimal;
 
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Szymon");
+        SpringApplication.run(App.class, args);
+    }
+    @Bean
+    ProductCatalog createNewProductCatalog() {
+        ProductCatalog productCatalog = new ProductCatalog(new HashMapProductStorage());
 
-        List<String> names = Arrays.asList("Jakub", "Michał", "Agnieszka", "Kasia");
-        Greeter greeter = new Greeter();
-        String[] namesAsArray = {"Jakub"};
+        String productId1 = productCatalog.addProduct("My ebok", "NIce one");
+        productCatalog.assignImage(productId1, "images/nice.jpeg");
+        productCatalog.changePrice(productId1, BigDecimal.TEN);
+        productCatalog.publishProduct(productId1);
 
-        ArrayList<String> ladies = new ArrayList<>();
+        String productId2 = productCatalog.addProduct("New Ebook", "NIce one");
+        productCatalog.assignImage(productId2, "images/nice.jpeg");
+        productCatalog.changePrice(productId2, BigDecimal.valueOf(20.20));
+        productCatalog.publishProduct(productId2);
 
-        names.stream()
-                .filter(name -> name.endsWith("a"))
-                .filter(name -> name.startsWith("A"))
-                .map(String::toUpperCase)
-                .forEach(greeter::greet);
+        return productCatalog;
     }
 }
---
+++ src/main/resources/static/scripts/index.js
@@ -0,0 +1,3 @@
+(() => {
+    console.log("My ebook store works");
+})();
